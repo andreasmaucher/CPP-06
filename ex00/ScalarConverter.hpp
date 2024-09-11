19f18860d33fc20d276ab356064757149998593b
amaucher <andreas-maucher@gmx.de>
2024-09-11 22:01:25 +0200

    first setup

---
+++ ex00/ScalarConverter.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amaucher <amaucher@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/23 14:22:27 by amaucher          #+#    #+#             */
+/*   Updated: 2024/09/11 21:58:43 by amaucher         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERTER_HPP
+
+# define SCALARCONVERTER_HPP
+
+#include <iostream>
+
+class ScalarConverter
+{	
+	public:
+		
+	private:
+		
+};
+
+#endif
